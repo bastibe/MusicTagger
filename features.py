3e5d33991592f3b933a8215767506359764daca5
Bastian Bechtold <basti@bastibe.de>
2013-08-31 12:07:05 +0200

    added docstrings to features

--- features.py
+++ features.py
@@ -4,22 +4,26 @@ import scipy.signal
 
 
 def rms(data, fft_data):
+    """Calculates a measure for the loudness of the data."""
     return np.sqrt(np.mean(data*data))
-    
+
 
 def peak(data, fft_data):
-    return np.max(np.abs(data))    
+    """Calculated the maximum value in the data."""
+    return np.max(np.abs(data))
 
 
-def crest(data, fft_data):
+def crest_factor(data, fft_data):
+    """Calculates the ratio of peak to rms of the data."""
     peak_val = peak(data, fft_data)
     rms_val = rms(data, fft_data)
     if peak_val == 0:
         return 1
     return peak_val/rms_val
 
-        
+
 def spectral_centroid(data, fft_data):
+    """Calculates the balance point of the spectrum."""
     freq = np.fft.fftfreq(len(data))[:len(fft_data)]
     freq[-1] += 1 # last element needs to be 0.5 not -0.5
     spec = np.abs(fft_data)
@@ -31,34 +35,39 @@ def spectral_centroid(data, fft_data):
 
 
 def log_spectral_centroid(data, fft_data):
+    """Calculates the logarithmic balance point of the spectrum."""
     freq = np.fft.fftfreq(len(data))[:len(fft_data)]
     freq[-1] += 1 # last element needs to be 0.5 not -0.5
-    freq_log = np.log(freq+1) # linear range into 1 to 1.5 
+    freq_log = np.log(freq+1) # linear range into 1 to 1.5
     spec = np.abs(fft_data)
     spec_sum = np.sum(spec)
     if spec_sum == 0:
         return 3*np.log(1.5) - 1 # the mean of the log freq-axes
     return np.sum(spec*freq_log)/spec_sum
-    
-    
+
+
 def spectral_variance(data, fft_data):
+    """Calculates the variance of the spectrum."""
     return np.var(np.abs(fft_data))
-        
-    
+
+
 def spectral_skewness(data, fft_data):
+    """Calculate the skewness of the spectrum."""
     return scipy.stats.skew(np.abs(fft_data))
-    
-    
+
+
 def spectral_flatness(data, fft_data):
+    """Calculates a measure for the flatness of the spectrum."""
     spec = np.abs(fft_data)
     spec_mean = np.mean(spec)
     spec_gmean = scipy.stats.gmean(spec)
     if spec_mean == 0:
         return 1
     return spec_gmean/spec_mean
-    
+
 
 def spectral_brightness(data, fft_data):
+    """Calculates the ratio between high and low frequencies."""
     spec = np.abs(fft_data)
     weight_vec = np.log(np.linspace(1, 100, len(fft_data)))
     weight_vec = np.pi*weight_vec/weight_vec[-1]
@@ -69,9 +78,9 @@ def spectral_brightness(data, fft_data):
             return 1 # attention: if signal is a sine at fs/2 this is also hit
     return high_spec_sum/low_spec_sum
 
-    
+
 def spectral_abs_slope_mean(data, fft_data):
+    """Calculates a measure for the variability of the spectrum."""
     spec = np.abs(fft_data)
     slope = np.abs(np.diff(spec))
     return np.mean(slope)
-    
