557e170ba1b87686c53709d74c1dcba2674cd644
Bastian Bechtold <basti@bastibe.de>
2013-09-10 18:13:13 +0200

    improved readability by using splice access to SoundFile

--- preprocess_sample_base.py
+++ preprocess_sample_base.py
@@ -8,7 +8,7 @@ import pdb
 
 def walk_files_preprocess(path_raw, path_processed):
     """ walk all files in raw path, return those names and
-    further return the names of the corresponding processed 
+    further return the names of the corresponding processed
     files
     """
     for file_path_raw in walk_files(path_raw):
@@ -18,26 +18,24 @@ def walk_files_preprocess(path_raw, path_processed):
             os.makedirs(file_dir_processed)
         yield(file_path_raw, file_path_processed)
 
-        
+
 def preprocess_sample(path_raw, path_processed):
-    """ process the sample of the sample base to be 
-    cutted to contain no silence and to be normalized 
+    """ process the sample of the sample base to be
+    cutted to contain no silence and to be normalized
     to a defined rms value
-    
-    """ 
+
+    """
     fs = 44100
     sound_file = SoundFile(path_raw, mode=read_mode)
     if sound_file.sample_rate != fs:
         print("Wrong sample rate of ", path_raw, "  ", sound_file.sample_rate, " should be ", fs)
         return
-    sound_file.seek_absolute(0)
-    data = sound_file.read(sound_file.frames)
-    data = preprocess_sample_data(data)
+    data = preprocess_sample_data(sound_file[:])
     wave_file_float = snd_types['WAV']|snd_subtypes['FLOAT']|snd_endians['FILE']
     sound_file = SoundFile(path_processed, sample_rate=fs, channels=1, format=wave_file_float, mode=write_mode)
     sound_file.write(data)
-    
-    
+
+
 def preprocess_sample_data(data):
     """ all desired preprocessing steps of the audio
     data of one sample
@@ -46,13 +44,13 @@ def preprocess_sample_data(data):
     data = remove_silence(data)
     return normalize_rms(data)
 
-    
+
 def pan_to_mono(data):
     """ merge several audio channels into one channel
     """
     return np.mean(data, axis=1)
-    
-    
+
+
 def remove_silence(data, silence_threshold_db=-75):
     """ remove silence at the beginning and end
     of an audio file saved in data
@@ -61,25 +59,24 @@ def remove_silence(data, silence_threshold_db=-75):
     start_idx = np.argmax(data > silence_threshold)
     end_idx = len(data)-np.argmax(data[::-1] > silence_threshold)-1
     return data[start_idx:end_idx]
-    
-    
+
+
 def normalize_rms(data, rms_desired=0.5):
     """ set the rms value of an audio file to the defined
     value
     """
     rms_actual = rms(data)
     return rms_desired*data/rms_actual
-    
-    
+
+
 def db_to_lin(value_db):
-    """ calculate linear value corresponding to 
+    """ calculate linear value corresponding to
     given value in decibel
     """
     return 10**(value_db*0.05)
-    
-    
+
+
 if __name__ == '__main__':
     sample_path_raw = 'SampleBase'
     sample_path_processed = 'SampleBaseProcessed'
     [preprocess_sample(*pathes) for pathes in walk_files_preprocess(sample_path_raw, sample_path_processed)]
-    
