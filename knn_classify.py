e1ac0542dc31da1329dea8d65fb86b1bf0d07a27
Bastian Bechtold <basti@bastibe.de>
2013-09-13 18:54:59 +0200

    fixed knn_classify and added command line interface

--- knn_classify.py
+++ knn_classify.py
@@ -1,19 +1,30 @@
+import pickle
 import os
 import numpy as np
 import pandas as pd
 from collections import Counter
-from preprocess_sample_base import preprocess_sample
-from extract_features import extract_features
+from extract_features import extract_features_pca
 from dynamic_time_warping import dtw_distance_c
+from docopt import docopt
 
+
+__doc__ = """
+Usage: knn_classify.py FILE [-k NUM_NEIGHBORS] [-f HD5_FILE] [-p PICKLE_FILE]
+
+Options:
+-h --help         show this message
+-k NUM_NEIGHBORS  the number of neighbors to consider [default: 10]
+-f HD5_FILE       file name where feature data is stored [default: feature_data.hd5]
+-p PICKLE_FILE    file name where the PCA object is stored [default: pca.pickle]
+"""
 
 def k_nearest_neighbors(sample_features, feature_data, k=5):
     ''' returns the tags and the probability of them within
     the k nearest neighbors
     '''
     feature_index = np.arange(5)
-    neighbors = [ (dtw_distance_c(sample_features[feature_index], 
-                                  sample2_features[feature_index]), 
+    neighbors = [ (dtw_distance_c(sample_features[feature_index],
+                                  sample2_features[feature_index]),
                    sample2_features['tag'].iloc[0])
                  for _, sample2_features in feature_data.groupby('file')]
     neighbors.sort(key = lambda x: x[0])
@@ -23,11 +34,16 @@ def k_nearest_neighbors(sample_features, feature_data, k=5):
 
 
 if __name__ == '__main__':
-    test_file_name = sys.argv[1]
-    features_file_name = sys.argv[2] if len(sys.argv) > 2 else 'feature_data.hd5'
-    pca_name = sys.argv[3] if len(sys.argv) > 3 else 'pca.pickle'
+    opt = docopt(__doc__)
+    k = int(opt['-k'])
+    test_file_name = opt['FILE']
+    features_file_name = opt['-f']
+    pca_name = opt['-p']
 
-    with open(pca_name, 'rb'): pca = pickle.read()
+    with open(pca_name, 'rb') as f: pca = pickle.load(f)
     file_features = extract_features_pca(test_file_name, pca)
-    features_data = pd.read_hdf(features_file_name, 'pca')
-    print(k_nearest_neighbors(file_features, feature_data, k=20))
+    feature_data = pd.read_hdf(features_file_name, 'pca')
+    neighbors = k_nearest_neighbors(file_features, feature_data, k=10)
+
+    for name, probability in neighbors:
+        print('%s: %i%%' % (name, int(probability*100)))
